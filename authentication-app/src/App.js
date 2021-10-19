869c6f7ced0c1da0389afc891da77a16f37df44c
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-19 12:25:40 +0300

    added react-router and react-router-dom
    and public-page moved to a separate page

--- authentication-app/src/App.js
+++ authentication-app/src/App.js
@@ -1,38 +1,15 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import PublicPage from "./pages/public-page"
+import {Route, Switch} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 function App() {
     return (
         <div className="App">
-            <Container className="mt-5">
-                <Row className="mt-5">
-                    <Col md={6} className={"textCenter"}>
-                        <h4 className="text-primary">
-                            Public page
-                        </h4>
-                    </Col>
-                    <Col md={6} className="float-end">
-                        <Button className="btn btn-success float-end">Admin Page</Button>
-                    </Col>
-                </Row>
-                <Row className="mt-5 float-none">
-                    <Col md={6} style={{height: "100px",backgroundColor:"#6930c3"}} className=""></Col>
-                    <Col md={1} style={{height: "100px"}}></Col>
-                    <Col md={5} style={{height: "100px",backgroundColor:"#5390d9"}}></Col>
-                </Row>
-                <Row className="mt-5">
-                    <Col md={12} style={{height: "200px",backgroundColor:"#80ffdb"}}></Col>
-
-                </Row>
-                <Row className="mt-5">
-                    <Col md={8} style={{height: "150px",backgroundColor:"#9d4edd"}}></Col>
-                    <Col md={1} style={{height: "150px"}}></Col>
-                    <Col md={3} style={{height: "150px",backgroundColor:"#48bfe3"}}></Col>
-                </Row>
-                <Row className="mt-5">
-                    <Col md={12} style={{height: "200px",backgroundColor:"#40916c"}}></Col>
-                    </Row>
-            </Container>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact  path="/" render={(props) => <PublicPage {...props}/>}/>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
